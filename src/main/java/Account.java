cf1087e507808d874e08bd781d80e8622a20e356
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2024-04-15 20:04:59 +0200

    finished without browser tests files

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+
 public class Account {
 
     private final String name;
@@ -11,6 +13,11 @@ public class Account {
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
+        Assert.assertTrue(name.length() >=3); // проверка длины
+        Assert.assertTrue(name.length() <=19); // проверка длины
+        Assert.assertFalse(name.endsWith(" ")); // проверка наличие пробела в конце строки
+        Assert.assertFalse(name.startsWith(" ")); // проверка наличие пробела в начале строки
+        Assert.assertTrue(name.matches("^\\s+$")); // проверка наличие пробела внутри
         return true;
 
     }
